8b1949132a86dcf4a36ffe54ec1ee70fb645f2e8
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-14 11:37:16 +0200

    snack 1

--- src/org/lessons/java/javasnacks.java
+++ src/org/lessons/java/javasnacks.java
@@ -1,10 +1,22 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class javasnacks {
 	public static void main(String[] args) {
 //		Snack1
 //		Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("inserisci un numero: ");
+		int nEvenOrOdd = sc.nextInt();
+		
+		if(nEvenOrOdd % 2 == 0) {
+			System.out.print(nEvenOrOdd);
+		} else{
+			System.out.println(nEvenOrOdd + 1);
+		}
+		
 //		Snack2
 //		Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
 
